6db632cc91eceeee6adc18090aaaf44c60d256e2
Volodymyr Lenyk <volodymyr.devops@gmail.com>
2024-06-05 22:48:33 +0300

    github_repository_file: add .gitignore and readme.md files

--- content.tf
+++ content.tf
@@ -17,3 +17,25 @@ resource "local_file" "gitignore" {
   content  = "*.tfstate\n*.tfstate.*\n*.tfvars\n.terraform/\n.terraform.lock.hcl\n"
   filename = "${path.module}/.gitignore"
 }
+
+resource "github_repository_file" "readme" {
+  repository          = var.project_repo_name
+  branch              = "main"
+  file                = "README.md"
+  content             = local_file.readme.content
+  commit_message      = "Managed by Terraform"
+  commit_author       = data.github_user.current.login
+  commit_email        = data.github_user.current.email
+  overwrite_on_create = true
+}
+
+resource "github_repository_file" "gitignore" {
+  repository          = var.project_repo_name
+  branch              = "main"
+  file                = ".gitignore"
+  content             = local_file.gitignore.content
+  commit_message      = "Managed by Terraform"
+  commit_author       = data.github_user.current.login
+  commit_email        = data.github_user.current.email
+  overwrite_on_create = true
+}
