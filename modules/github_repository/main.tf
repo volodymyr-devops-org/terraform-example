b991c1ba0c5d86cc29160e2958e98c333347ace9
Volodymyr Lenyk <volodymyr.devops@gmail.com>
2024-06-06 21:05:58 +0300

    github_branch_protection_defaults: add resource, add default vars

--- modules/github_repository/main.tf
+++ modules/github_repository/main.tf
@@ -20,3 +20,30 @@ resource "github_branch" "this" {
   repository = github_repository.this.name
   branch     = each.value.name
 }
+
+resource "github_branch_protection" "main" {
+
+  for_each   = { for b in var.repo_branches : b.name => b if b.enable_protection }
+  repository_id = github_repository.this.node_id
+
+  pattern                         = each.value.name
+  enforce_admins                  = var.github_branch_protection_defaults.enforce_admins
+  allows_deletions                = var.github_branch_protection_defaults.allows_deletions
+  allows_force_pushes             = var.github_branch_protection_defaults.allows_force_pushes
+  require_signed_commits          = var.github_branch_protection_defaults.require_signed_commits
+  require_conversation_resolution = var.github_branch_protection_defaults.require_conversation_resolution
+
+  required_status_checks {
+    strict = true
+  }
+
+  required_pull_request_reviews {
+    dismiss_stale_reviews           = var.github_branch_protection_defaults.required_pull_request_reviews.dismiss_stale_reviews
+    restrict_dismissals             = var.github_branch_protection_defaults.required_pull_request_reviews.restrict_dismissals
+    required_approving_review_count = var.github_branch_protection_defaults.required_pull_request_reviews.required_approving_review_count
+    require_last_push_approval      = var.github_branch_protection_defaults.required_pull_request_reviews.require_last_push_approval
+    pull_request_bypassers          = var.github_branch_protection_defaults.pull_request_bypassers
+  }
+
+  depends_on = [github_branch.this]
+}
--- modules/github_repository/variables.tf
+++ modules/github_repository/variables.tf
@@ -25,3 +25,30 @@ variable "repo_branches" {
     enable_protection = bool
   }))
 }
+
+variable "github_branch_protection_defaults" {
+  type = object({
+    enforce_admins                  = bool,
+    allows_deletions                = bool,
+    allows_force_pushes             = bool,
+    require_signed_commits          = bool,
+    require_conversation_resolution = bool,
+    required_pull_request_reviews   = map(string)
+    pull_request_bypassers          = set(string)
+  })
+
+  default = {
+    enforce_admins                  = true
+    allows_deletions                = false
+    allows_force_pushes             = false
+    require_signed_commits          = false
+    require_conversation_resolution = true
+    required_pull_request_reviews = {
+      dismiss_stale_reviews           = true
+      restrict_dismissals             = true
+      required_approving_review_count = 2
+      require_last_push_approval      = true
+    }
+    pull_request_bypassers = ["/volodymyr-devops"]
+  }
+}
